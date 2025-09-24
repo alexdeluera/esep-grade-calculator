fc10cb7cecbda17a50497f8fc1a3bfa2b19b60e7
Alex De Luera <134332668+alexdeluera@users.noreply.github.com>
2025-09-24 12:34:38 -0400

    Consolidate Grades to single list
    Update code to use a single list for all types of grades instead of three separate ones.

--- grade-calculator/grade_calculator.go
+++ grade-calculator/grade_calculator.go
@@ -1,9 +1,7 @@
 package esepunittests
 
 type GradeCalculator struct {
-	assignments []Grade
-	exams       []Grade
-	essays      []Grade
+	grades []Grade
 }
 
 type GradeType int
@@ -32,9 +30,7 @@ type Grade struct {
 
 func NewGradeCalculator() *GradeCalculator {
 	return &GradeCalculator{
-		assignments: make([]Grade, 0),
-		exams:       make([]Grade, 0),
-		essays:      make([]Grade, 0),
+		grades: make([]Grade, 0),
 	}
 }
 
@@ -56,44 +52,34 @@ func (gc *GradeCalculator) GetFinalGrade() string {
 }
 
 func (gc *GradeCalculator) AddGrade(name string, grade int, gradeType GradeType) {
-	switch gradeType {
-	case Assignment:
-		gc.assignments = append(gc.assignments, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Assignment,
-		})
-	case Exam:
-		gc.exams = append(gc.exams, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Exam,
-		})
-	case Essay:
-		gc.essays = append(gc.essays, Grade{
-			Name:  name,
-			Grade: grade,
-			Type:  Essay,
-		})
-	}
+	gc.grades = append(gc.grades, Grade{
+		Name:  name,
+		Grade: grade,
+		Type:  Assignment,
+	})
+
 }
 
 func (gc *GradeCalculator) calculateNumericalGrade() int {
-	assignment_average := computeAverage(gc.assignments)
-	exam_average := computeAverage(gc.exams)
-	essay_average := computeAverage(gc.essays)
+	assignment_average := gc.computeAverage(Assignment)
+	exam_average := gc.computeAverage(Exam)
+	essay_average := gc.computeAverage(Essay)
 
 	weighted_grade := assignment_average*0.5 + exam_average*0.35 + essay_average*0.15
 
 	return int(weighted_grade + 0.5)
 }
 
-func computeAverage(grades []Grade) float64 {
+func (gc *GradeCalculator) computeAverage(gradeType GradeType) float64 {
 	sum := 0
+	count := 0
 
-	for _, grade := range grades {
-		sum += grade.Grade
+	for _, g := range gc.grades {
+		if g.Type == gradeType {
+			sum += g.Grade
+			count++
+		}
 	}
 
-	return float64(sum) / float64(len(grades))
+	return float64(sum) / float64(count)
 }
